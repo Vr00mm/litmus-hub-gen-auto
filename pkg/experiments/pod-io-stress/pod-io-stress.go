05fd96ba15ab60b7b586cdb245714fab174028f6
Remi Ziolkowski <remi.ziolkowski-ext@pole-emploi.fr>
2021-11-04 10:21:29 +0100

    more more more

---
+++ pkg/experiments/pod-io-stress/pod-io-stress.go
@@ -0,0 +1,33 @@
+package podIOStress
+
+import (
+	"hub-gen-auto/pkg/requirements"
+	"hub-gen-auto/pkg/resources"
+	"hub-gen-auto/pkg/types"
+)
+
+var ExperimentsManifests map[string]types.ChaosChart
+
+var experimentRequirements = []string{"isRoot"}
+
+var availableLibs = []string{"litmus", "pumba"}
+
+func generateExperiment(composant resources.Object, container string) types.ChaosChart {
+	exp := ExperimentsManifests["pod-io-stress"]
+	exp.ChaosExperiment.Metadata.Name = "pod-io-stress-" + composant.GetName() + "-" + container
+	return exp
+}
+
+func checkRequirements(composant resources.Object) bool {
+	return requirements.CheckRequirements(experimentRequirements, composant)
+}
+
+func Generate(composant resources.Object) []types.ChaosChart {
+	var experiments []types.ChaosChart
+	containers, _ := composant.GetContainers()
+	for container := range containers {
+		exp := generateExperiment(composant, containers[container])
+		experiments = append(experiments, exp)
+	}
+	return experiments
+}
