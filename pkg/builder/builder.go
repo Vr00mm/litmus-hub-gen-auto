b5ce1a08007ce1c85dd287d3cd7cb15d31093fb6
Remi Ziolkowski <remi.ziolkowski-ext@pole-emploi.fr>
2021-11-04 12:44:09 +0100

    run

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -23,11 +23,17 @@ func writeToFile(filename string, data interface{}) {
 func Build(clusterName string, manifests []types.Manifest) error {
 	var err error
 	for _, manifest := range manifests {
+		os.MkdirAll("build/"+clusterName+"/"+manifest.Namespace+"/icons/", os.ModePerm)
+		writeToFile("build/"+clusterName+"/"+manifest.Namespace+"/"+clusterName+"-"+manifest.Namespace+".chartserviceversion.yaml", manifest.Chart)
+		writeToFile("build/"+clusterName+"/"+manifest.Namespace+"/"+clusterName+"-"+manifest.Namespace+".package.yaml", manifest.Package)
 		for _, experiment := range manifest.Experiments {
 			os.MkdirAll("build/"+clusterName+"/"+manifest.Namespace+"/"+experiment.ChaosExperiment.Metadata.Name+"/", os.ModePerm)
 			writeToFile("build/"+clusterName+"/"+manifest.Namespace+"/"+experiment.ChaosExperiment.Metadata.Name+"/experiment.yaml", experiment.ChaosExperiment)
 			writeToFile("build/"+clusterName+"/"+manifest.Namespace+"/"+experiment.ChaosExperiment.Metadata.Name+"/engine.yaml", experiment.ChaosEngine)
+			writeToFile("build/"+clusterName+"/"+manifest.Namespace+"/"+experiment.ChaosExperiment.Metadata.Name+"/rbac.yaml", experiment.RBAC)
+			writeToFile("build/"+clusterName+"/"+manifest.Namespace+"/"+experiment.ChaosExperiment.Metadata.Name+"/rbac-psp.yaml", experiment.PSP)
 			writeToFile("build/"+clusterName+"/"+manifest.Namespace+"/"+experiment.ChaosExperiment.Metadata.Name+"/"+experiment.ChaosExperiment.Metadata.Name+".chartserviceversion.yaml", experiment.ChaosExperiment)
+			writeToFile("build/"+clusterName+"/"+manifest.Namespace+"/icons/"+experiment.ChaosExperiment.Metadata.Name+".png", experiment.Icon)
 		}
 
 	}
