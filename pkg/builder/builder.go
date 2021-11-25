ea2b77280435b9f4cf8b5a92d8ae276627a0e7c2
Rémi ZIOLKOWSKI <remi.ziolkowski@gmail.com>
2021-11-25 13:52:02 +0100

    fix again

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -8,12 +8,12 @@ import (
 func Build(clusterName string, manifests []types.Manifest) error {
 	var err error
 	for _, manifest := range manifests {
-		utils.WriteToFileAsYaml("build/"+clusterName+"/charts/"+manifest.Namespace+"/"+manifest.Namespace+".chartserviceversion.yaml", manifest.Chart)
-		utils.WriteToFileAsYaml("build/"+clusterName+"/charts/"+manifest.Namespace+"/"+manifest.Namespace+".package.yaml", manifest.Package)
+		utils.WriteToFileAsYaml("build/"+clusterName+"/charts/"+clusterName+"-"+manifest.Namespace+"/"+manifest.Namespace+".chartserviceversion.yaml", manifest.Chart)
+		utils.WriteToFileAsYaml("build/"+clusterName+"/charts/"+clusterName+"-"+manifest.Namespace+"/"+manifest.Namespace+".package.yaml", manifest.Package)
 		for _, experiment := range manifest.Experiments {
 			utils.WriteToFileAsYaml("build/"+clusterName+"/charts/"+clusterName+"-"+manifest.Namespace+"/"+experiment.ChaosExperiment.Metadata.Name+"/experiment.yaml", experiment.ChaosExperiment)
 			utils.WriteToFileAsYaml("build/"+clusterName+"/charts/"+clusterName+"-"+manifest.Namespace+"/"+experiment.ChaosExperiment.Metadata.Name+"/engine.yaml", experiment.ChaosEngine)
-			utils.WriteToFileAsYaml("build/"+clusterName+"/charts/"+clusterName+"-"+clusterName+"-"+manifest.Namespace+"/"+experiment.ChaosExperiment.Metadata.Name+"/rbac.yaml", experiment.RBAC)
+			utils.WriteToFileAsYaml("build/"+clusterName+"/charts/"+clusterName+"-"+manifest.Namespace+"/"+experiment.ChaosExperiment.Metadata.Name+"/rbac.yaml", experiment.RBAC)
 			utils.WriteToFileAsYaml("build/"+clusterName+"/charts/"+clusterName+"-"+manifest.Namespace+"/"+experiment.ChaosExperiment.Metadata.Name+"/rbac-psp.yaml", experiment.PSP)
 			utils.WriteToFileAsYaml("build/"+clusterName+"/charts/"+clusterName+"-"+manifest.Namespace+"/"+experiment.ChaosExperiment.Metadata.Name+"/"+experiment.ChaosExperiment.Metadata.Name+".chartserviceversion.yaml", experiment.ChaosExperiment)
 			utils.WriteToFile("build/"+clusterName+"/charts/"+clusterName+"-"+manifest.Namespace+"/icons/"+experiment.ChaosExperiment.Metadata.Name+".png", string(experiment.Icon))
